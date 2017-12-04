942c7d384d4629be835d55d34599a789b3a65b62
naga bharat <naga@synycssolutions.com>
2017-12-04 23:02:55 +0530

    add

---
+++ src/ng2/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    UpgradeModule
+  ],
+  providers: [],
+ // bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) { }
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, ['phonecatApp'], { strictDi: true });
+  }
+}
